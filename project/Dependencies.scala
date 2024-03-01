263008d232676104db186f0e290bc931824cd69f
nmcb <nmcb@xs4all.nl>
2024-03-01 06:05:59 +0000

    Update scala3-library to 3.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   /** Language dependencies */
-  val ScalaLanguageVersion = "3.3.1"
+  val ScalaLanguageVersion = "3.3.3"
 
   /** Platform dependencies */
   val Http4sVersion        = "0.23.25"
